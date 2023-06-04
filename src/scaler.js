cecb13c170c8a06e61e4439578dafc1be748175a
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-04 05:19:42 -0400

    test 1 of aspect scaling

--- src/scaler.js
+++ src/scaler.js
@@ -1,4 +1,8 @@
 // Upscales CDO project, this will have to be dynamic depending on the projects aspect ratio
 const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
-const embed = document.querySelector("#game")
+const embed = document.querySelector("#game");
 embed.src = urlEmbed;
+const aspect = Math.min(window.innerWidth, window.innerHeight);
+const scale = aspect / embed.style["width"].match(/\d+/g)[0]; // divide this by the height if you want the joypad
+embed.style["transform-origin"] = `top center`;
+embed.style["transform"] = `scale(${scale})`;
