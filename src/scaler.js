caa43a14d400af89d44f6ced8764d905524a8de8
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-04 02:29:47 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -1,4 +1,4 @@
 // Upscales CDO project, this will have to be dynamic depending on the projects aspect ratio
-const urlEmbed = `${location.search.match(/(?<=id=)[^&]*/g)}/embed`
+const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game")
 embed.src = urlEmbed;
