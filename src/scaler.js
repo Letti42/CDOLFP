f5268bfa2d4d503009692a33007c9b3699cbd499
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-04 06:38:48 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -1,10 +1,9 @@
 // Upscales CDO project, this will have to be dynamic depending on the projects aspect ratio
 const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
-const frame = document.querySelector("#visualization > div")
+const frame = document.querySelector("#visualizationColumn > div > div")
 embed.src = urlEmbed;
-console.log(embed);
 const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / frame.style["width"].match(/\d+/g)[0]; // divide this by the height if you want the joypad
+const scale = aspect / frame.clientWidth; // divide this by the height if you want the joypad
 embed.style["transform"] = `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
