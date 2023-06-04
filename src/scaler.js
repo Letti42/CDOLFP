1dd58323abff4e31ff41feecc14ca1341a7d3f6e
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-04 09:02:06 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -3,6 +3,13 @@ const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
 embed.src = urlEmbed;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / 408; // divide this by the height if you want the joypad
+if(urlEmbed.includes("gamelab")) {
+  embed.width = "440px";
+  embed.height = "757px";
+} else {
+  embed.width = "392px";
+  embed.height = "620px";
+}
+const scale = aspect / embed.width; // divide this by the height if you want the joypad
 embed.style["transform"] = `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
