02f69c128e5a3fa7cd961ff57b22dec42a6b4519
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-04 09:21:08 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -3,6 +3,6 @@ const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
 embed.src = urlEmbed;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / 408; // divide this by the height if you want the joypad
+const scale = aspect / 405; // divide this by the height if you want the joypad
 embed.style["transform"] = `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
